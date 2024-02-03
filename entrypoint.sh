c6daebcc42cbba1099575ff82df9ebdef9c73abb
Toshiya-Kato <toshiya.kato@linecorp.com>
2024-02-03 15:24:41 +0900

    initial update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,22 @@
 set -e
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
-  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
   git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+echo "${INPUT_ENVVAR}" | while IFS= read -r line; do
+  export line
+done
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" \
-      -name="linter-name (misspell)" \
+terraform init
+terraform validate -json \
+  | jq -r '.diagnostics[] | "\(.range.filename):\(.range.start.line):\(.range.start.column): \(.detail)"' \
+  | reviewdog -efm="%f:%l:%c:%m" \
+      -name="terraform validate" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+      "${INPUT_REVIEWDOG_FLAGS}"
