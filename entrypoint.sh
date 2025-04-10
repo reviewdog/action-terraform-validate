e865a28acb13f4e9bb3d524013fd496b5f436350
H.IWAYAMA <1152796+hiwayama@users.noreply.github.com>
2025-04-10 15:49:38 +0900

    OpenTofu support (#57)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 TERRAFORM_VERSION=$1
+CLI=$2
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
@@ -16,9 +17,30 @@ case "${UNAME_ARCH}" in
   *)         echo "Unsupported architecture: ${UNAME_ARCH}. Only AMD64 and ARM64 are supported by the action" && exit 1
 esac
 
-wget -q "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_${ARCH}".zip \
-    && unzip "./terraform_${TERRAFORM_VERSION}_linux_${ARCH}.zip" -d /usr/local/bin/ \
-    && rm -rf "./terraform_${TERRAFORM_VERSION}_linux_${ARCH}.zip"
+downloadTerraform() {
+  wget -q "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_${ARCH}".zip \
+      && unzip "./terraform_${TERRAFORM_VERSION}_linux_${ARCH}.zip" -d /usr/local/bin/ \
+      && rm -rf "./terraform_${TERRAFORM_VERSION}_linux_${ARCH}.zip"
+}
+downloadOpenTofu() {
+  wget -q "https://github.com/opentofu/opentofu/releases/download/v${TERRAFORM_VERSION}/tofu_${TERRAFORM_VERSION}_linux_${ARCH}".zip \
+      && unzip "./tofu_${TERRAFORM_VERSION}_linux_${ARCH}.zip" -d /usr/local/bin/ \
+      && rm -rf "./tofu_${TERRAFORM_VERSION}_linux_${ARCH}.zip"
+}
+
+case "${CLI}" in
+  terraform)
+    COMMAND=terraform
+    downloadTerraform
+    ;;
+  opentofu)
+    COMMAND=tofu
+    downloadOpenTofu
+    ;;
+  *) 
+    echo "Unsupported command: ${CLI}. Only terraform and OpenTofu are supported by the action" && exit 1
+    ;;
+esac
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 export TF_TOKEN_app_terraform_io="${INPUT_TERRAFORM_CLOUD_TOKEN}"
@@ -37,9 +59,9 @@ jq_script='
 }
 '
 
-terraform init -backend=false
+$COMMAND init -backend=false
 # shellcheck disable=SC2086
-terraform validate -json \
+$COMMAND validate -json \
   | jq "$jq_script" -c \
   | reviewdog -f="rdjsonl" \
       -name="${INPUT_NAME}" \
